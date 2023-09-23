c268f5aabdda5996229c76846b30c9e962f34ebf
shivamsingh <shvmvhsshukra@gmail.com>
2023-09-23 20:27:49 +0530

    First commit

---
+++ app/src/main/java/shivam/learns/collection/LearnList.java
@@ -0,0 +1,20 @@
+package shivam.learns.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+
+
+ */
+
+public class LearnList {
+    public static void main(String[] args) {
+        List<Integer> integerArrayList = new ArrayList<>();
+        List<Integer> integerLinkedList = new LinkedList<>();
+
+
+
+    }
+}
