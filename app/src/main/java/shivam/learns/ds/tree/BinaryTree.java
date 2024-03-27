fb647f8d760bf3130f8a64ca71d3840959f1823b
shivamsingh <shvmvhsshukra@gmail.com>
2024-03-27 23:55:48 +0530

    Added vertical order traversal

--- app/src/main/java/shivam/learns/ds/tree/BinaryTree.java
+++ app/src/main/java/shivam/learns/ds/tree/BinaryTree.java
@@ -8,9 +8,23 @@ import shivam.learns.ds.Node;
 
 import java.util.*;
 
+class Tupple {
+    public Node node;
+    public int x,y;
+    public Tupple(Node root, int x, int y){
+        this.node = root;
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class BinaryTree {
     Node root;
     static Scanner sc;
+    static Queue<Node> queue = new ArrayDeque<>();
+
+    static TreeMap<Integer,TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<Integer,TreeMap<Integer, PriorityQueue<Integer>>>();
+
 
     public BinaryTree() {
         root = null;
@@ -61,47 +75,47 @@ public class BinaryTree {
         sc = new Scanner(System.in);
         Node root = createTree();
         System.out.println("Printing the tree in PreOrder traversal");
-        printTreePreOrder(root);
+        preOrderTraversal(root);
         System.out.println();
         System.out.println("Printing the tree in PreOrder traversal");
-        printTreeInOrder(root);
+        inOrderTraversal(root);
         System.out.println();
         System.out.println("Printing the tree in PreOrder traversal");
-        printTreePostOrder(root);
+        postOrderTraversal(root);
         System.out.println();
         System.out.println("Printing the tree in Level order traversal");
-        printTreeLevelOrder(root);
+        levelOrderTraversalIterative(root);
+        System.out.println();
+        System.out.println("Printing Vertical Order - ");
+        verticalOrderTraversal(root);
 
     }
 
-    private static void printTreePreOrder(Node root) {
+    private static void preOrderTraversal(Node root) {
         if(root == null)
             return;
         System.out.print(root.getVal()+" ");
-        printTreePreOrder(root.getLeft());
-        printTreePreOrder(root.getRight());
+        preOrderTraversal(root.getLeft());
+        preOrderTraversal(root.getRight());
     }
-    private static void printTreeInOrder(Node root) {
+    private static void inOrderTraversal(Node root) {
         if(root == null)
             return;
-        printTreeInOrder(root.getLeft());
+        inOrderTraversal(root.getLeft());
         System.out.print(root.getVal()+" ");
-        printTreeInOrder(root.getRight());
+        inOrderTraversal(root.getRight());
     }
-
-    private static void printTreePostOrder(Node root) {
+    private static void postOrderTraversal(Node root) {
         if(root == null)
             return;
-        printTreePostOrder(root.getLeft());
-        printTreePostOrder(root.getRight());
+        postOrderTraversal(root.getLeft());
+        postOrderTraversal(root.getRight());
         System.out.print(root.getVal()+" ");
     }
-
-    static Queue<Node> queue = new ArrayDeque<>();
-
-    private static void printTreeLevelOrder(Node root) {
+    private static void levelOrderTraversal(Node root) {
         if(root == null)
             return;
+        queue.add(root);
         System.out.println(root.getVal()+" ");
         if(root.getLeft() != null)
             queue.add(root.getLeft());
@@ -109,7 +123,51 @@ public class BinaryTree {
             queue.add(root.getRight());
         while(!queue.isEmpty()){
             Node node = queue.poll();
-            printTreeLevelOrder(node);
+            levelOrderTraversalIterative(node);
+        }
+    }
+    private static void levelOrderTraversalIterative(Node root) {
+        Queue<Node> queue = new ArrayDeque();
+        if(root == null)
+            return;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            System.out.println(node.getVal()+" ");
+            if(node.getLeft() != null)
+                queue.add(node.getLeft());
+            if(node.getRight() != null)
+                queue.add(node.getRight());
+        }
+    }
+    private static void verticalOrderTraversal(Node root) {
+        ArrayDeque<Tupple> queue = new ArrayDeque<>();
+        if(root == null)
+            return;
+        queue.add(new Tupple(root, 0, 0));
+        while(!queue.isEmpty()){
+            Tupple tupple = queue.poll();
+            if(!map.containsKey(tupple.x))
+                map.put(tupple.x,new TreeMap<>());
+            if(!map.get(tupple.x).containsKey(tupple.y))
+                map.get(tupple.x).put(tupple.y,new PriorityQueue<>());
+            map.get(tupple.x).get(tupple.y).offer(tupple.node.getVal());
+            if(tupple.node.getLeft()!=null)
+                queue.add(new Tupple(tupple.node.getLeft(),tupple.x-1,tupple.y+1));
+            if(tupple.node.getRight()!=null)
+                queue.add(new Tupple(tupple.node.getRight(),tupple.x+1,tupple.y+1));
+
+        }
+        List<List<Integer>> answer = new LinkedList<>();
+        for(TreeMap<Integer, PriorityQueue<Integer>> ys: map.values() ){
+            List<Integer> temp = new LinkedList<>();
+            for(PriorityQueue<Integer> q : ys.values()){
+                while(!q.isEmpty()){
+                    System.out.println(q.peek());
+                    temp.add(q.poll());
+                }
+                answer.add(temp);
+            }
         }
     }
 }
